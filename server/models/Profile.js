cd62f1c394aa57a2e0e9a2bb608c2ebedf6b8024
nakuln23 <nakulnagargade@gmail.com>
2019-05-31 17:15:23 +0530

    Updated profile model

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,7 +1,64 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema
 
-const profileSchema = new mongoose.Schema({
-  description: String
+const profileSchema = new Schema({
+  user : {
+    type:Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  bio:{
+    type:String
+  },
+  location:{
+    type: String,
+  },
+  skills:{
+    type:[String],
+    required: true,
+  },
+  experience : {
+    title:{
+      type: String,
+      required : true
+    },
+    company:{
+      type:String,
+      required: true
+    }
+  },
+  education : {
+    school:{
+      type: String,
+      required : true
+    },
+    degree:{
+      type:String,
+      required: true
+    },
+    fieldofstudy:{
+      type:String
+    },
+    current : {
+      type: Boolean,
+      Default: false
+    },
+  },
+  social: {
+    twitter :{
+      type: String ,
+    },
+    facebook :{
+      type: String ,
+    },
+    linkedin :{
+      type: String ,
+    },    
+  },
+  date :{
+    type: Date,
+    default: Date.now
+  }
+
 });
 
 const Profile = mongooose.model("Profile", profileSchema);
